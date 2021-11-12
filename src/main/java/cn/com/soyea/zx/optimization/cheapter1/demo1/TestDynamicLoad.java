dce5b2c2231bab0db7d162c5e6d36a78fc6af4d4
soyeaCommnet <17346850383@163.com>
2021-11-12 09:11:18 +0800

    #添加类加载机制学习 #添加jvm调优

---
+++ src/main/java/cn/com/soyea/zx/optimization/cheapter1/demo1/TestDynamicLoad.java
@@ -0,0 +1,19 @@
+package cn.com.soyea.zx.optimization.cheapter1.demo1;
+
+/**
+ * 类加载机制详解
+ *
+ * @author zx
+ * @date 2021/11/11 15:19
+ */
+public class TestDynamicLoad {
+    static {
+        System.out.println("##############load TestDynamicLoad############");
+    }
+
+    public static void main(String[] args) {
+        new A();
+        System.out.println("#######load test ############");
+        B b = null;
+    }
+}
